605f425aac24df137a7fb892f8314238792e4dff
Karan2210 <karan.prabhu2210@gmail.com>
2017-07-17 00:35:12 -0400

    feat[module-client]- Module 2 submission

---
+++ client/src/app/services/alerts.service.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Dell on 7/12/2017.
+ */
+(function() {
+    'use strict';
+
+    angular.module('plunker')
+        .service('alertsService', alertsService);
+
+    alertsService.$inject = ['$http', '$q', 'CONFIG'];
+
+    function alertsService($http, $q, CONFIG) {
+
+        var self = this;
+
+        self.getAlertsById = getAlertsById;
+
+        function getAlertsById(id) {
+            return $http.get(CONFIG.API_HOST + '/alerts/' + id)
+                .then(successFn, errorFn);
+        }
+
+        function successFn(response) {
+            return response.data;
+        }
+
+        function errorFn(response) {
+            return $q.reject('ERROR: ' + response.statusText);
+        }
+    }
+
+})();
